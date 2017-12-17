b893d8eae501f38362cfafaf6583e51d9b861ef3
Mohammad Amin Zeynali <office_zeinaly@yahoo.com>
2017-12-18 02:58:44 +0330

    create database

---
+++ Divar.Core/Entities/UserIdentity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Divar.Core.Entities
+{
+	public class UserIdentity : IdentityUser
+	{
+		[StringLength(100)]
+		public string DisplayName { get; set; }
+	}
+}
---
+++ Divar.Infrastructure/Database/ContextDb.cs
@@ -0,0 +1,19 @@
+﻿using Divar.Core.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Divar.Infrastructure.Database
+{
+	public class ContextDb : IdentityDbContext<UserIdentity>
+	{
+		public ContextDb()
+			: base("name=Divar")
+		{
+
+		}
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
