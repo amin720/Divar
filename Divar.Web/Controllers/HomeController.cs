ee50e8016227876b244c9127190cffbe151cf3fd
Mohammad Amin Zeynali <office_zeinaly@yahoo.com>
2017-12-20 17:33:18 +0330

    add elmah, home controller, farsi font

---
+++ Divar.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Divar.Web.Controllers
+{
+	[RoutePrefix("")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+		[Route("")]
+		[HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
