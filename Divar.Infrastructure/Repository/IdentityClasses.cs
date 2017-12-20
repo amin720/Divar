33a4f918181e49db476fab5ff18f33a1e15f42a5
Mohammad Amin Zeynali <office_zeinaly@yahoo.com>
2017-12-20 16:43:11 +0330

    add user , role

---
+++ Divar.Infrastructure/Repository/IdentityClasses.cs
@@ -0,0 +1,29 @@
+﻿using Divar.Core.Entities;
+using Divar.Infrastructure.Database;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Divar.Infrastructure.Repository
+{
+	public class CmsUserStore : UserStore<UserIdentity>
+	{
+		public CmsUserStore()
+			: this(new ContextDb())
+		{ }
+		public CmsUserStore(ContextDb context)
+			: base(context)
+		{ }
+
+	}
+
+	public class CmsUserManager : UserManager<UserIdentity>
+	{
+		public CmsUserManager()
+			: this(new CmsUserStore())
+		{ }
+
+		public CmsUserManager(UserStore<UserIdentity> userStore)
+			: base(userStore)
+		{ }
+	}
+}
---
+++ Divar.Web/App_Start/AuthDbConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Divar.Core.Entities;
+using Divar.Infrastructure.Repository;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Divar.Web.App_Start
+{
+	public class AuthDbConfig
+	{
+		public static async Task RegisterAdmin()
+		{
+			using (var users = new UserRepository())
+			{
+				var user = await users.GetUserByNameAsync("admin");
+
+				if (user == null)
+				{
+					var adminUser = new UserIdentity
+					{
+						UserName = "admin",
+						Email = "admin@cms.com",
+						DisplayName = "Administrator"
+					};
+
+					await users.CreateAsync(adminUser, "User@1234");
+				}
+
+				await users.AddUserToRoleAsync(user, "admin");
+			}
+
+			using (var roles = new RoleRepository())
+			{
+				if (await roles.GetRoleByNameAsync("admin") == null)
+				{
+					await roles.CreateAsync(new IdentityRole("admin"));
+				}
+
+				if (await roles.GetRoleByNameAsync("editor") == null)
+				{
+					await roles.CreateAsync(new IdentityRole("editor"));
+				}
+
+				if (await roles.GetRoleByNameAsync("author") == null)
+				{
+					await roles.CreateAsync(new IdentityRole("author"));
+				}
+
+				if (await roles.GetRoleByNameAsync("user") == null)
+				{
+					await roles.CreateAsync(new IdentityRole("user"));
+				}
+			}
+		}
+	}
+}
